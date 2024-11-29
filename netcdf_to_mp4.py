b016d89fc0f524c192c43f7f7578918d47fefd4f
LeMayday <21mayc@gmail.com>
2024-11-29 15:40:44 -0500

    improved readability

--- netcdf_to_mp4.py
+++ netcdf_to_mp4.py
@@ -11,10 +11,8 @@ import cv2
 
 ################################# USER INPUTS #################################
 
-# netcdf file directory
-nc_dir = './bin'
-
-frame_duration = 0.1 # seconds per frame
+nc_dir = './bin'            # netcdf file directory
+frame_duration = 0.1        # seconds per frame
 
 ###############################################################################
 
@@ -22,7 +20,7 @@ frame_duration = 0.1 # seconds per frame
 filenums = range(201)
 # add leading zeros
 filenums = [str(item).zfill(3) for item in filenums]
-# variables of interest
+# variables of interest (2 or 3 corresponds to out2 or out3)
 variables2 = ['rho', 'press', 'vel1', 'vel2', 'r0', 'vapor1']
 units2 = ['kg/m^3', 'Pa', 'm/s', 'm/s', '', ''] # r0 and vapor1 are ratios
 variables3 = ['temp', 'h2o', 'h2oc', 'co2', 'co2c']
@@ -31,19 +29,21 @@ units3 = ['K', '', '', '', '']
 variables = variables2 + variables3
 units = units2 + units3
 
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+fourcc = cv2.VideoWriter_fourcc(*'mp4v') # mp4 codec
 
 for var in variables:
     output_gif = './geyser_%s.gif' %var # output gif file
     output_mp4 = './geyser_%s.mp4' %var # output mp4 file
     filenames = []
     for num in filenums:
+        # choose right file depending on variable
         if var in variables2:
             file = '%s/geyser.out2.00%s.nc' %(nc_dir, num)
         elif var in variables3:
             file = '%s/geyser.out3.00%s.nc' %(nc_dir, num)
         nc_file = netCDF4.Dataset(file)
 
+        # plot image
         fig,ax = plt.subplots(figsize=(4.5,6)) # adjust image aspect to your data
         img = ax.imshow(nc_file[var][0,:,:,0], cmap='jet', origin='lower')
         cbar = fig.colorbar(img)
@@ -51,10 +51,10 @@ for var in variables:
         ax.set_title(var + (' (' + unit + ')' if unit != '' else ''))
         ax.tick_params(axis='x', colors='white')
         ax.tick_params(axis='y', colors='white')
-
-        file = '%s_%s.png' %(var, num)
         fig.tight_layout(pad=0.4)
+
         # save to temp png file
+        file = '%s_%s.png' %(var, num)
         fig.savefig(file)
         plt.close()
         filenames.append(file)
